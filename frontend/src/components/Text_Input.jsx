3c6b00e7a2cf3a4f58ffc3486aebed76530df31a
Ali Karaoglu <psyak24@exmail.nottingham.ac.uk>
2025-08-01 11:43:28 +0300

    backend environment created

--- frontend/src/components/Text_Input.jsx
+++ frontend/src/components/Text_Input.jsx
@@ -3,7 +3,26 @@ import React, { useState } from "react";
 import '../css/Text_Input.css'
 
 export default function Text_Input() {
+
+
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(message)
+      setMessage("")
+    }
+    
+  
   return (
-      <textarea className="client-request" placeholder="  Type Here..."></textarea>
+    <form onSubmit={handleSubmit} className="input-form">
+      <textarea
+        className="client-request"
+        placeholder="  Type Here..."
+      ></textarea>
+      <button type="submit" className = "button type1">
+        <span className = "btn-txt">Submit</span>
+      </button>
+    </form>
   );
 }
